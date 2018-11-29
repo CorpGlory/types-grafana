aed508378302a7f1948eca9a7b2b99a8a4d3fb65
Krishna Teja Mannam <mkrishnateja123@gmail.com>
2018-11-29 15:43:17 +0530

    Added typings for constants file

---
+++ app/core/constants.d.ts
@@ -0,0 +1,13 @@
+export const GRID_CELL_HEIGHT: number;
+export const GRID_CELL_VMARGIN: number;
+export const GRID_COLUMN_COUNT: number;
+export const REPEAT_DIR_VERTICAL: string;
+
+export const DEFAULT_PANEL_SPAN: number;
+export const DEFAULT_ROW_HEIGHT: number;
+export const MIN_PANEL_HEIGHT: number;
+
+export const LS_PANEL_COPY_KEY: string;
+
+export const DASHBOARD_TOOLBAR_HEIGHT: number;
+export const DASHBOARD_TOP_PADDING: number;
