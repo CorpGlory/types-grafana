1e6c02651fc7d08a437f98ef3bba911dbdf3de8b
Willis Chou (EXT-TW) <willis_chou@trendmicro.com>
2018-12-27 14:05:42 +0800

    Add TemplateSrv

--- app/features/templating/all.d.ts
+++ app/features/templating/all.d.ts
@@ -7,4 +7,5 @@ import { DatasourceVariable } from './datasource_variable';
 import { CustomVariable } from './custom_variable';
 import { ConstantVariable } from './constant_variable';
 import { AdhocVariable } from './adhoc_variable';
-export { VariableSrv, IntervalVariable, QueryVariable, DatasourceVariable, CustomVariable, ConstantVariable, AdhocVariable };
+import { TemplateSrv } from './template_srv';
+export { VariableSrv, IntervalVariable, QueryVariable, DatasourceVariable, CustomVariable, ConstantVariable, AdhocVariable, TemplateSrv };
