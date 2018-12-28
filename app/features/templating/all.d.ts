6568c982efbc5531007991eced25c51b22a165e5
Willis Chou (EXT-TW) <willis_chou@trendmicro.com>
2018-12-28 22:00:24 +0800

    Remove templateSrv from all.d.ts to respect grafana implementation

--- app/features/templating/all.d.ts
+++ app/features/templating/all.d.ts
@@ -7,6 +7,5 @@ import { DatasourceVariable } from './datasource_variable';
 import { CustomVariable } from './custom_variable';
 import { ConstantVariable } from './constant_variable';
 import { AdhocVariable } from './adhoc_variable';
-import { TemplateSrv } from './template_srv';
 
-export { VariableSrv, IntervalVariable, QueryVariable, DatasourceVariable, CustomVariable, ConstantVariable, AdhocVariable, TemplateSrv };
+export { VariableSrv, IntervalVariable, QueryVariable, DatasourceVariable, CustomVariable, ConstantVariable, AdhocVariable };
