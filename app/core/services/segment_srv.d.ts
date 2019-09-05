6c6257174ecf57cac2410499b9867d6489ec8093
rozetko <rozetko@corpglory.com>
2019-09-05 18:51:28 +0300

    Types for app/core/services/segment_srv.ts (#13)

---
+++ app/core/services/segment_srv.d.ts
@@ -0,0 +1,36 @@
+// app/core/services/segment_srv.ts is not exposed to plugins
+import { TemplateSrv } from '../../features/templating/template_srv';
+
+export declare class UiSegmentSrv {
+  /** @ngInject */
+  constructor($sce: any, templateSrv: TemplateSrv);
+  getSegmentForValue(value: string, fallbackText: string): MetricSegment;
+  newSelectMeasurement(): MetricSegment;
+  newFake(text: string, type: string, cssClass: string): MetricSegment;
+  newSegment(options: MetricSegment): MetricSegment;
+  newKey(key: string): MetricSegment;
+  newKeyValue(value: string): MetricSegment;
+  newCondition(condition: string): MetricSegment;
+  newOperator(op: string): MetricSegment;
+  newOperators(ops: string[]): MetricSegment[];
+  transformToSegments(
+    addTemplateVars: boolean,
+    variableTypeFilter?: string
+  ): (results: { text: string, expandable?: boolean }[]) => MetricSegment[];
+  newSelectMetric(): MetricSegment;
+  newPlusButton(): MetricSegment;
+}
+
+export declare class MetricSegment {
+  value: string;
+  html: string;
+  type: string;
+  expandable: boolean;
+  text: string;
+  cssClass: string;
+  fake: boolean;
+  custom: boolean;
+  selectMode: boolean;
+
+  constructor(options: string | MetricSegment);
+}
