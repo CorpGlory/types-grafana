69e76995557a76976716b1fb7167ed73f89ed4db
Aleksandr Volochnev <a.volochnev@gmail.com>
2020-02-24 10:54:06 +0100

    Added app/features/datasources/utils/passwordHandlers.ts

---
+++ app/features/datasources/utils/passwordHandlers.ts
@@ -0,0 +1,15 @@
+export enum PasswordFieldEnum {Password = 'password', BasicAuthPassword = 'basicAuthPassword'}
+export type Ctrl = {
+  current: {
+    secureJsonFields: {
+      [key: string]: boolean;
+    };
+    secureJsonData?: {
+      [key: string]: string;
+    };
+    password?: string;
+    basicAuthPassword?: string;
+  };
+};
+export const createResetHandler = (ctrl: Ctrl, field: PasswordFieldEnum) => (event: any) => {};
+export const createChangeHandler = (ctrl: any, field: PasswordFieldEnum) => (event: any) => {};
